d04e45502339962013cdaab6ee025c8e8a6934c9
Sai_Sarmi <139688376+Sarmilanking@users.noreply.github.com>
2024-12-09 16:17:17 +0530

    Update code  bookid.php

--- 06_12/bookid.php
+++ 06_12/bookid.php
@@ -1,1 +1,17 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        require_once 'dbconf.php';
+        require_once 'myfunc.php';
+        $id=$_GET['bookid'];
+        GetBook($id,$connect);
+    ?>
+</body>
+</html>
