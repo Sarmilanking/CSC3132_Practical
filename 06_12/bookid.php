40bfe2e5f1fc2d24fa52a449a279ca5e914af039
Sai_Sarmi <139688376+Sarmilanking@users.noreply.github.com>
2024-12-10 07:42:16 +0000

    recoded

--- 06_12/bookid.php
+++ 06_12/bookid.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,8 +15,27 @@
         GetBook($id,$connect);
     ?>
 </body>
-<<<<<<< HEAD
+
 </html>
-=======
+
 </html>
->>>>>>> 06d6a87 (created bookid.php)
+
+<?php
+define('SERVERNAME', '127.0.0.1');
+define('USERNAME', 'root');
+define('PASSWORD', 'mariadb');
+define('DBNAME', 'book');
+try {
+	$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+	if (!$connect) {
+		die("connection failed".mysqli_connect_error());
+	} 
+	else {
+		echo "Connected successfully<br>";
+	}
+} 
+catch (Exception $e) {
+	die($e->getMessage());
+}
+
+?>
