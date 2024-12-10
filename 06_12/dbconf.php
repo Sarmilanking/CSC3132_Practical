bd2647f05405a562c940baa179616f6926cf425d
Sai_Sarmi <139688376+Sarmilanking@users.noreply.github.com>
2024-12-10 07:45:30 +0000

    recoded 1

--- 06_12/dbconf.php
+++ 06_12/dbconf.php
@@ -2,26 +2,8 @@
 define('SERVERNAME', '127.0.0.1');
 define('USERNAME', 'root');
 define('PASSWORD', 'mariadb');
-define('DBNAME', 'school');
-try {<?php
-	define('SERVERNAME', '127.0.0.1');
-	define('USERNAME', 'root');
-	define('PASSWORD', 'mariadb');
-	define('DBNAME', 'book');
-	try {
-		$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
-		if (!$connect) {
-			die("connection failed".mysqli_connect_error());
-		} 
-		else {
-			echo "Connected successfully<br>";
-		}
-	} 
-	catch (Exception $e) {
-		die($e->getMessage());
-	}
-	
-	?>
+define('DBNAME', 'book');
+try {
 	$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
 	if (!$connect) {
 		die("connection failed".mysqli_connect_error());
