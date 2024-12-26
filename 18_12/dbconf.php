18286519f7a338b86c8b9fc224daf343fa99b68b
Sai_Sarmi <139688376+Sarmilanking@users.noreply.github.com>
2024-12-26 14:08:17 +0000

    db reconnected

--- 18_12/dbconf.php
+++ 18_12/dbconf.php
@@ -1,15 +1,11 @@
 <?php
 //constant variable (variable name, value)
-/*define('SERVERNAME', '127.0.0.1:3306');
+define('SERVERNAME', '127.0.0.1:3306');
 define('USERNAME', 'root');
-define('PASSWORD', 'mariadb');*/
-define('SERVERNAME','localhost');
-define('USERNAME','root');
-define('PASSWORD','');
-define('DBNAME', 'library');
+define('PASSWORD', 'mariadb');
+define('DBNAME','library');
 try{
-	
-$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+	$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
 
 if (!$connect) {
 	die("connection failed".mysqli_connect_error()); //die - stop process after that
